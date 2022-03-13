474514b9644ab3ea4043da0173d5a4e422881661
Julian Dunskus <julian.dunskus@gmail.com>
2022-03-13 05:09:42 +0100

    allow http

--- EffectCompiler/Startup.cs
+++ EffectCompiler/Startup.cs
@@ -44,7 +44,7 @@ namespace EffectCompiler
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EffectCompiler v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
