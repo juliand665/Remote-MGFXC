9f7605b7ddae201718bd8794f5fb7bbbc90e2a16
Julian Dunskus <julian.dunskus@gmail.com>
2022-04-22 07:21:41 +0200

    handle includes!

--- remote_mgfxc.py
+++ remote_mgfxc.py
@@ -4,8 +4,11 @@
 # if you're using fish, i'd recommend using `set -Ux REMOTE_MGFXC_HOST <hostname>` to set it permanently
 # you can also set $REMOTE_MGFXC_PORT--the port defaults to 44321 if unset
 
-import sys
+from io import FileIO
 import os
+import sys
+import re
+from typing import Dict
 import requests
 
 host = os.getenv("REMOTE_MGFXC_HOST")
@@ -31,7 +34,57 @@ class Style:
     bright_red = "\033[91m"
 
 
-for input_filename in filenames:
+def print_styled(text: str, style: str):
+    print(style, end="")
+    print(text, end="")
+    print(Style.reset)
+
+
+# read input files
+files: Dict[str, str] = {}
+
+
+def read_file(filename: str):
+    with open(filename, "r", encoding="utf-8-sig") as file:
+        files[filename] = file.read()
+
+
+for filename in filenames:
+    read_file(filename)
+
+root_filenames = set(filenames)
+
+
+def inline_includes(filename: str):
+    code = files[filename]
+    rewritten_code = ""
+    include_regex = r'\s*#include\s+"([^"]+)"\s*'
+    for line in code.split("\n"):
+        match = re.match(include_regex, line)
+        if match:
+            include_filename = match.group(1)
+            if include_filename in root_filenames:
+                root_filenames.remove(include_filename)
+            if include_filename not in files:
+                read_file(include_filename)
+
+            inline_includes(include_filename)
+
+            rewritten_code += f"// begin included file: {include_filename}\n"
+            rewritten_code += files[include_filename] + "\n"
+            rewritten_code += f"// end included file: {include_filename}\n"
+            rewritten_code += "\n"
+        else:
+            rewritten_code += line + "\n"
+    files[filename] = rewritten_code
+
+
+# inline included files
+for filename in filenames:
+    inline_includes(filename)
+
+
+for input_filename in root_filenames:
     print()
     # basename with stripped .fx extension
     basename, extension = os.path.splitext(os.path.basename(input_filename))
@@ -43,26 +96,30 @@ for input_filename in filenames:
         and os.path.exists(output_filename)
         and (os.path.getmtime(output_filename) > os.path.getmtime(input_filename))
     ):
-        print(f"{Style.bright_black}Skipping {input_filename}{Style.reset}")
+        print_styled(f"Skipping {input_filename}", Style.bright_black)
         continue
 
     # remove previous output
     if os.path.exists(output_filename):
         os.remove(output_filename)
 
-    print(f"{Style.bold}Compiling {input_filename}{Style.reset}")
+    print_styled(f"Compiling {input_filename}", Style.bold)
 
     # run the compiler
     url = f"http://{host}:{port}/compiler?filename={basename}"
-    response = requests.post(url, data=open(input_filename, "rb"))
+    # print(files[input_filename])
+    response = requests.post(url, data=files[input_filename].encode("utf-8"))
+
     if response.status_code == 200:
-        with open(output_filename, "wb") as f:
-            f.write(response.content)
-        print(f"{Style.green}Successfully compiled!{Style.reset}")
-    if response.status_code == 500:
+        with open(output_filename, "wb") as output_file:
+            output_file.write(response.content)
+        print_styled("Successfully compiled!", Style.green)
+    elif response.status_code == 500:
         error_json = response.json()
         title = error_json["title"]
         detail = error_json["detail"]
-        print(f"{Style.bright_red}{title} for {input_filename}:{Style.reset}")
-        print(f"{Style.red}{detail}{Style.reset}")
+        print_styled(f"{title} for {input_filename}:", Style.bright_red)
+        print_styled(detail, Style.red)
+    else:
+        print_styled(f"Unknown error code: {response.status_code}", Style.bright_red)
 print()
