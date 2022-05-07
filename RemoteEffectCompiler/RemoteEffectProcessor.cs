6591b638e410f8077a0813d32316e4b100252779
Julian Dunskus <julian.dunskus@gmail.com>
2022-05-07 23:20:16 +0200

    handle includes & defines in pipeline extension!

--- RemoteEffectCompiler/RemoteEffectProcessor.cs
+++ RemoteEffectCompiler/RemoteEffectProcessor.cs
@@ -3,6 +3,8 @@ using System.ComponentModel;
 using System.IO;
 using System.Net.Http;
 using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Text.RegularExpressions;
 using Microsoft.Xna.Framework.Content.Pipeline;
 using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
 using Microsoft.Xna.Framework.Content.Pipeline.Processors;
@@ -33,25 +35,57 @@ namespace RemoteEffectCompiler
 			var client = new HttpClient { Timeout = TimeSpan.FromSeconds(10) };
 			var filename = Path.GetFileNameWithoutExtension(input.Identity.SourceFilename);
 			var builder = new UriBuilder(scheme: "http", host: Host, port: int.Parse(Port), pathValue: $"/compiler");
-			builder.Query = $"defines={Defines}";
-			var response = client.PostAsync(
-				builder.Uri,
-				new StringContent(input.EffectCode))
-				.Result;
+			builder.Query = $"defines={Defines ?? ""};";
+			var inlined = FileWithInlinedIncludes(input.Identity.SourceFilename);
+			var response = client.PostAsync(builder.Uri, new StringContent(inlined)).Result;
 
 			if (!response.IsSuccessStatusCode)
 			{
-				//var jsonString = response.Content.ReadAsStringAsync().Result;
-				//var details = JsonConvert.DeserializeObject<ProblemDetails>(jsonString);
-				var details = new DataContractJsonSerializer(typeof(ProblemDetails))
-					.ReadObject(response.Content.ReadAsStreamAsync().Result) as ProblemDetails;
+				string errorDescription;
+				if (response.Content.Headers.ContentType.MediaType == "application/json")
+				{
+					var details = new DataContractJsonSerializer(typeof(ProblemDetails))
+						.ReadObject(response.Content.ReadAsStreamAsync().Result) as ProblemDetails;
+					errorDescription = details.detail;
+				}
+				else
+				{
+					errorDescription = response.Content.ReadAsStringAsync().Result;
+				}
 				throw new InvalidContentException(
-					$"Remote compiler failed with status code {response.StatusCode}:\n{details.detail}",
+					$"Remote compiler failed with status code {response.StatusCode}:\n{errorDescription}",
 					input.Identity);
 			}
 
 			var bytecode = response.Content.ReadAsByteArrayAsync().Result;
 			return new(bytecode);
+		}
+
+		string FileWithInlinedIncludes(string path)
+		{
+			var lines = File.ReadAllLines(path);
+			var builder = new StringBuilder();
+			foreach (var line in lines)
+			{
+				if (line.StartsWith("#include"))
+				{
+					const string includeRegex = @"#include\s+""(?<path>[^""]+)""";
+					var match = Regex.Match(line, includeRegex);
+					if (!match.Success)
+						throw new InvalidContentException(
+							$"Invalid include directive: {line}",
+							contentIdentity: new(path));
+					var relativePath = match.Groups["path"].Value;
+					var baseFolder = Path.GetDirectoryName(path);
+					var absolutePath = Path.Combine(baseFolder, relativePath);
+					builder.Append(FileWithInlinedIncludes(absolutePath));
+				}
+				else
+				{
+					builder.AppendLine(line);
+				}
+			}
+			return builder.ToString();
 		}
 	}
 }
